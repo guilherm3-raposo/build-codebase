0d8c9a44bcf68e4cd2398bab7d6438be7c392b03
Guilherme Raposo <guilherme.raposo.85@gmail.com>
2023-08-07 01:06:01 -0300

    Runs build on child process

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 use std::process::Command;
 
 pub fn build(project: String) {
-    let _ = Command::new(format!("./builders/{}.sh", project)).output();
+    let _ = Command::new(format!("./builders/{}.sh", project)).spawn();
 }
